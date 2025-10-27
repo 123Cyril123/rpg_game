59abc56030ac6f78bfe006a885fe4ca00c53c740
Cyril Hrdlicka <cyril.hrdlicka@gmail.com>
2025-10-27 12:48:08 +0100

    Player movement

---
+++ main.cpp
@@ -0,0 +1,94 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+
+int main()
+{	//-------------INITIALIZE-----------------------------
+	ContextSettings settings;
+	settings.antiAliasingLevel = 8;
+	RenderWindow window(VideoMode({1980,1050}), "RPG Game", Style::Default, State::Fullscreen, settings);
+
+	
+
+
+
+
+	//-------------INITIALIZE-----------------------------
+
+	//-------------LOAD-----------------------------
+	Texture playerTexture;
+	Sprite playerSprite(playerTexture);
+	
+
+	if (playerTexture.loadFromFile("Project1/assets/player/textures/spriteSheet.png"))
+	{	
+		int XIndex = 0;
+		int YIndex = 0;
+
+		playerSprite.setTexture(playerTexture);
+		playerSprite.setTextureRect(IntRect({XIndex*64, YIndex*64}, {64,64}));
+		playerSprite.scale({ 2,2 });
+	}
+	else {
+		cout << "Image failed to load" << endl;
+	}
+
+
+
+	
+
+
+	//-------------LOAD-----------------------------
+	while (window.isOpen()) 
+	{	
+
+
+		//----------UPDATE--------------------------------
+		while (optional<Event> event = window.pollEvent())
+		{
+			if (event->is<Event::Closed>())
+			{
+				window.close();
+
+			}
+			else if (event->is<Event::KeyPressed>())
+			{
+				const auto* key = event->getIf<Event::KeyPressed>();
+
+				if (key->scancode == Keyboard::Scan::Escape)
+				{
+					window.close();
+				}
+			}
+		}
+		Vector2f playerPosition = playerSprite.getPosition();
+		int speed = 4;
+		if (Keyboard::isKeyPressed(Keyboard::Scan::W))
+		{	
+			playerSprite.setPosition({ playerPosition.x, playerPosition.y - speed });
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Scan::A))
+		{	
+			playerSprite.setPosition({ playerPosition.x - speed, playerPosition.y });
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Scan::S))
+		{	
+			playerSprite.setPosition({ playerPosition.x, playerPosition.y + speed});
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Scan::D))
+		{	
+
+			playerSprite.setPosition({ playerPosition.x + speed, playerPosition.y });
+		}
+		//----------UPDATE--------------------------------
+
+		//----------DRAW----------------------------------
+		window.clear(Color::Black);
+		window.draw(playerSprite);
+		window.display();
+		//----------DRAW----------------------------------
+
+	}
+	return 0;
+}
